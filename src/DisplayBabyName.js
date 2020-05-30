43d8938f1e163c787d23d48d3769331aa6f3c8ba
minwwgo <37987965+minwwgo@users.noreply.github.com>
2020-05-31 00:01:34 +0100

    search babyName

--- src/DisplayBabyName.js
+++ src/DisplayBabyName.js
@@ -1,24 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import "./DisplayBabyName.css";
+
 const DisplayBabyName = (props) => {
-  const sortBabyNames = props.results.sort((a, b) =>
-    a.name > b.name ? 1 : -1
-  );
+  const{results}=props
+  const [data, setData] = useState(results);
+  const [value, setValue] = useState("");
+  
+
+  results.sort((a, b) => (a.name > b.name ? 1 : -1));
+
+  const handlerChange = (inputData) => {
+    setValue(inputData);
+    const searchValue = inputData.toLowerCase();
+    setData(
+        results.filter((babyInfo) =>
+          babyInfo.name.toLowerCase().includes(searchValue)
+        )
+      );
+  };
+  const removeName =(data)=>{
+
+  }
+
 
   return (
-    
-    <div className = "holder">
-    
-      {sortBabyNames.map((babyInfo) => (
+    <div>
+      <div className="input-holder">
+        <form>
+          <input
+            className="input"
+            placeholder="search by"
+            value={value}
+            onChange={(e) => handlerChange(e.target.value)}
+          />
+        </form>
+      </div>
+
+      <div className="holder">
+        {data.map((babyInfo) => (
           <div key={babyInfo.id} className="name-holder">
             <div className={babyInfo.sex === "m" ? "ele male " : "ele female"}>
               {babyInfo.name}
             </div>
           </div>
         ))}
+      </div>
     </div>
-      
-    
   );
 };
 export default DisplayBabyName;
